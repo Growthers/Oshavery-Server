db31bb80bfab02bb220079be48276c5a49a74d8a
sobonn <sobonn.8921@gmail.com>
2021-08-31 19:55:00 +0900

    [feat] /guildsと/guilds/:guildIdの実装

---
+++ src/controllers/guildcontroller.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import { guild } from "../models/guild";
+
+export const guildController = {
+  async getGuild(req: express.Request, res: express.Response) {
+    console.log(req.path);
+
+    await guild.get(req.params.guildId)
+      .then((gld) => {
+        console.log(gld);
+        res.json(gld);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(404).end();
+      })
+
+  },
+
+  async createGuild(req: express.Request, res: express.Response) {
+    const body = req.body;
+
+    console.log(req.path);
+
+    await guild.create(body)
+      .then((gld) => {
+        console.log(gld);
+        res.status(201).json(gld);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(400).send("Invaild reqest");
+      });
+  },
+
+  async updateGuild(req: express.Request, res: express.Response) {
+    const guild_id = req.params.guildId;
+    const body = req.body;
+
+    console.log(req.path);
+
+    await guild.update(guild_id, body)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(400).send("Invalid reqest");
+      });
+  },
+
+  async deleteGuild(req: express.Request, res: express.Response) {
+    console.log(req.path);
+    await guild.delete(req.params.guildId)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(400).send("Invalid reqest");
+      });
+  }
+}
