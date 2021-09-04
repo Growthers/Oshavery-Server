460f48f4d54b27d2e6e2ed9e2eb9de625525858f
sigumaa <siguma95@gmail.com>
2021-09-04 13:45:24 +0900

    fix/channel

--- src/controllers/channelcontroller.ts
+++ src/controllers/channelcontroller.ts
@@ -5,14 +5,26 @@ export const channelController = {
 
   async getChannels(req: express.Request, res: express.Response) {
     console.log(req.path);
-    const channel_data = await channels.get();
-    console.log(channel_data);
-    res.json(channel_data);
+    const guild_id = req.params.guildId;
+
+    await channels.get(guild_id)
+      .then((ch) => {
+        console.log(ch);
+        res.status(200).json(ch);
+        return;
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(400).send("Bad Request");
+        return;
+      });
+    return;
   },
 
   async createChannel(req: express.Request, res: express.Response) {
     console.log(req.path);
     const body = req.body;
+    const guild_id = req.params.guildId;
 
     const channel: channel = {
       channel_name: body.channel_name,
@@ -21,9 +33,9 @@ export const channelController = {
       channel_position: body.channel_position
     };
 
-    await channels.create(channel)
-      .then((r) => {
-        res.status(201).json(r);
+    await channels.create(channel,guild_id)
+      .then((ch) => {
+        res.status(201).json(ch);
         return;
       })
       .catch((e) => {
