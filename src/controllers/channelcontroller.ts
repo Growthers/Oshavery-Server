e25da47b4e7749b8ec695020e776235ec08b8db4
sigumaa <siguma95@gmail.com>
2021-09-12 11:35:27 +0900

    fix

--- src/controllers/channelcontroller.ts
+++ src/controllers/channelcontroller.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { channels,channel } from "../models/channel";
-import { channelCreated } from "./notificationcontroller";
+import { channelCreated } from "./notificationController";
 export const channelController = {
 
   async getChannels(req: express.Request, res: express.Response) {
