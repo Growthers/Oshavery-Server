4bb9a6e8abee62ae6aff1f2c72caa06bd0c79027
Tatsuto Yamamoto <laminne33569@approvers.dev>
2021-10-01 16:57:37 +0900

    fix add chalk

---
+++ src/config/config.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+type instanceConfig = {
+
+
+
+}
+
+// コンフィグファイルを読み込む
+async function loadConfig(){
+  const config = fs.readFileSync("./.config/config.json");
+
+
+
+
+}
+
+loadConfig();
