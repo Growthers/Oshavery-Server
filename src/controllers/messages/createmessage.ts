ab5c33869e73c672e818ffbd8219533cc38754cd
Tatsuto Yamamoto <laminne33569@approvers.dev>
2021-10-28 23:59:38 +0900

    refactor: createmessage.tsからAuth0依存部分を剥がす

--- src/controllers/messages/createmessage.ts
+++ src/controllers/messages/createmessage.ts
@@ -11,21 +11,7 @@ export async function createMessage(req: any, res: FastifyReply) {
   const now = new Date();
   // ToDo: ユーザーIP取得の廃止
   const ip_address = req.headers["x-forwaded-for"] || "";
-  let author;
-
-  if (process.env.NODE_ENV === "production") {
-    // トークンを取る
-    const token = req.headers.authorization || "";
-    // デコード
-    // eslint-disable-next-line
-    const decoded: any = await jwt_decode(token);
-    const { sub } = decoded;
-
-    // トークンから得られるSubを使ってユーザー検索
-    author = await users.getFromSub(sub);
-  } else {
-    author = await users.getFromSub("oshavery|1");
-  }
+  const author = await users.getFromSub("oshavery|1");
 
   if (!author) {
     return res.status(404).send("Not found");
