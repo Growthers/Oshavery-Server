3d72447039361fe0d1f440f76c67d450f6751597
Tatsuto Yamamoto <laminne33569@approvers.dev>
2021-11-30 23:13:07 +0900

    fix: 不要なTodoの削除

--- src/controllers/guilds/deleteguild.ts
+++ src/controllers/guilds/deleteguild.ts
@@ -1,23 +1,12 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { GuildIdParam } from "../../types/guild_types";
-import { deleteGuild } from "../../service/guild/delete";
+import { deleteGuild as dG } from "../../service/guild/delete";
 
-export type guild = {
-  id: string; // id
-  name: string; // ギルド名
-  topic: string; // トピック
-  icon: string; // アイコンのURL
-  created_at: Date; // 作成日時
-  updated_at?: Date; // アップロード日時
-  deleted_at?: Date; // 削除日時(使えるのかは未検証
-};
-
-export async function DeleteGuild(
+export async function deleteGuild(
   req: FastifyRequest<{ Params: GuildIdParam }>,
   res: FastifyReply
 ) {
-  // Todo: Serviceに投げる
-  const resp = await deleteGuild(req.params.guildId);
+  const resp = await dG(req.params.guildId);
 
   if (resp !== null) {
     return res.status(204).send();
--- src/service/guild/delete.ts
+++ src/service/guild/delete.ts
@@ -1,7 +1,6 @@
 import { deleteGuild as dG } from "../../repositories/guild";
 
 export async function deleteGuild(id: string) {
-  // ToDo: データとってくる
   const res = await dG(id);
   if (!res) {
     return null;
